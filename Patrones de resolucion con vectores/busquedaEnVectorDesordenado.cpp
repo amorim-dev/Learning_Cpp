263d61043579fd8d1bb44c179d0cc12c1dba3037
Bianca Amorim <amorim.bia.dev@gmail.com>
2021-06-03 23:55:01 -0300

    Procesa el vector ingresado por teclado

--- Patrones de resolucion con vectores/busquedaEnVectorDesordenado.cpp
+++ Patrones de resolucion con vectores/busquedaEnVectorDesordenado.cpp
@@ -2,27 +2,28 @@
 
 #include <iostream>
 using namespace std;
-#define CANT 10
+#define CANT 100
 
 typedef int tVector[CANT];
 int buscarV(tVector, int, int);
-void cargarVector(tVector, int);
+void crearVector(tVector, int &);
 void mostrarVector(tVector, int);
 
 int main()
 {
     int i;
     tVector vect1;
+    int kTOPE;
     setlocale(LC_CTYPE, "Spanish");
-    cargarVector(vect1, CANT);
-    mostrarVector(vect1, CANT);
+    crearVector(vect1, kTOPE);
+    mostrarVector(vect1, kTOPE);
 
     while (char buscarNumero = 's')
     {
         buscarNumero = 's';
         int numero = 0;
 
-        cout << "Ingrese el numero que deseas buscar: " << endl;
+        cout << "Ingrese el numero que deseas buscar en el vector: " << endl;
         cin >> numero;
 
         i = buscarV(vect1, numero, CANT);
@@ -47,16 +48,26 @@ int buscarV(tVector vect1, int dato, int kTOPE)
     return i;
 }
 
-void cargarVector(tVector vect1, int kTOPE)
+void crearVector(tVector vect1, int &kTOPE)
 {
-    cout << "datos del vector" << endl;
-    for (int i = 0; i < kTOPE; i++)
-        vect1[i] = 2 * i;
+    int cantidadPosicionesVector = 0;
+    cout << "Ingrese los datos del vector separados (0 para finalizar)" << endl;
+
+    do
+    {
+        cin >> vect1[cantidadPosicionesVector];
+        cantidadPosicionesVector++;
+
+    } while (vect1[cantidadPosicionesVector - 1] != 0);
+    kTOPE = cantidadPosicionesVector - 1;
 }
 
 void mostrarVector(tVector vect1, int kTOPE)
 {
-    for (int i = 0; i < kTOPE; i++)
-        cout << vect1[i] << " - ";
+    if (kTOPE == 0)
+        return;
+    cout << "Su vector es " << vect1[0];
+    for (int i = 1; i < kTOPE; i++)
+        cout << " - " << vect1[i];
     cout << endl;
 }
